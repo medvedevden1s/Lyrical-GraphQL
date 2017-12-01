b4c78d6a371655072dcc12a858068f35b24b9572
Denys Medvedev <d.medvedev@levi9.com>
2017-12-01 18:41:39 +0200

    - Fix appolo client - added first react component

--- client/index.js
+++ client/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient form 'apollo-client';
-import { ApolloProvider} from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
-const client = new ApolloProvider({});
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   );
 };
