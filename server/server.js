51c3b06e6d4cdf7949dda16e861297b458539468
Denys Medvedev <d.medvedev@levi9.com>
2017-12-01 18:19:15 +0200

    - add mongo

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:admin@ds127126.mlab.com:27126/apollo';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
