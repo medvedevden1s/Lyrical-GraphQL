3fe17ce116a5d85e3e7b9a54bdd4866e610d9046
medvedevden1s <medvedevdenis12@gmail.com>
2017-12-03 22:48:41 +0200

    - added likes

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-// import gql from 'graphql-tag';
-// import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 // import { Link } from 'react-router';
 // import query from '../queries/fetchSongs';
 
 class LyricList extends Component {
 
-  // onSongDelete(id) {
-  //   this.props.mutate({ variables: { id } })
-  //     .then(() => this.props.data.refetch());
-  // }
-
+  onLike(id) {
+    this.props.mutate({
+      variables: { id }
+    })
+  }
+  //.then(() => this.props.data.refetch());
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className={"collection-item"}>
           {content}
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id)}>thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
@@ -24,22 +32,19 @@ class LyricList extends Component {
   render() {
     // if (this.props.data.loading) { return <div>Loading....</div> }
     return (
-        <ul className="collection">
-          {this.renderLyrics()}
-        </ul>
+      <ul className="collection">
+        {this.renderLyrics()}
+      </ul>
     );
   }
 }
 
-// const mutation = gql`
-// mutation DeleteSong($id: ID){
-//     deleteSong(id: $id) {
-//         id
-//     }
-// }`;
+const mutation = gql`
+mutation LikeLyric($id: ID){
+    likeLyric(id: $id) {
+        id
+        likes
+    }
+}`;
 
-// export default graphql(mutation)(
-//   graphql(query)(LyricList)
-// );
-
-export default (LyricList);
+export default graphql(mutation)(LyricList);
