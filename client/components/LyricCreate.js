064ef9bb620fc33138a4d0d149da59ed4048cae3
medvedevden1s <medvedevdenis12@gmail.com>
2017-12-03 22:06:00 +0200

    - Added lyrics to the song

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+// import { Link, hashHistory } from 'react-router';
+// import fetchSong from '../queries/fetchSong';
+
+class LyricCreate extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { content: '' };
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        this.props.mutate({
+            variables: {
+                content: this.state.content,
+                songId: this.props.songId
+            }
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <label>Add Lyric:</label>
+                <input
+                    value={this.state.content}
+                    onChange={e => this.setState({ content: e.target.value })}
+                />
+            </form>
+        );
+    }
+}
+
+const mutation = gql`
+mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+        id
+        lyrics {
+            content
+        }
+    }
+}
+`;
+
+export default graphql(mutation)(LyricCreate);
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-// import gql from 'graphql-tag';
 import { Link, hashHistory } from 'react-router';
 import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
 
 class SongDetail extends Component {
     render() {
-
-
         const { song } = this.props.data; // if (this.props.data.loading) { return <div>Loading....</div> }
         if (!song) { return <div>Loading....</div> } // If it's quite fast do not display anything at all as a case
 
         return (
             <div>
-                <h3>{song.title}</h3>
                 <Link to="/">Back</Link>
-                {/*<form onSubmit={this.onSubmit.bind(this)}>
-                    <label>Song Title:</label>
-                    <input
-                        onChange={e => this.setState({ title: e.target.value })}
-                        value={this.state.title}
-                    />
-                </form> */}
+                <h3>{song.title}</h3>
+                <LyricCreate songId={this.props.params.id}/>
             </div>
         );
     }
