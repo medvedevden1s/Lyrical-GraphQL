8dc09b4ede1510f3b3f9e68cd7c0c802ee21b596
medvedevden1s <medvedevdenis12@gmail.com>
2017-12-03 19:08:30 +0200

    !!!! Refetch query added (If you added item to the array apollo not going to render query again by default)

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
     constructor(props) {
@@ -14,12 +15,9 @@ class SongCreate extends Component {
         e.preventDefault();
 
         this.props.mutate({
-            variables: {
-                title: this.state.title
-            }
-        }).then(() => {
-            hashHistory.push('/');
-        });
+            variables: { title: this.state.title },
+            refetchQueries: [{ query }]
+        }).then(() => hashHistory.push('/'));
     }
 
     render() {
